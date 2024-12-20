e8572d5a3a6d935baa923a50ca0a891d66026490
Leonardo Mazoco <115808428+leomazoco@users.noreply.github.com>
2024-12-20 16:35:24 -0300

    new

--- app/models/aeronave.py
+++ app/models/aeronave.py
@@ -1,14 +1,14 @@
 from app import db
 
 class Veiculo(db.Model):
-    __tablename__ = 'veiculos'
+    __tablename__ = 'aeronaves'
 
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     modelo = db.Column(db.String(80), nullable = False)
     marca = db.Column(db.String(80), nullable = False)
     tipo = db.Column(db.String(80), nullable = False)
     capacidade = db.Column(db.String(80), nullable = False)
-    veiculo = db.Column(db.String(50), nullable = False)
+    tipo_veiculo = db.Column(db.String(50), nullable = False)
     preco = db.Column(db.Double(10, 2))
     imagem = db.Column(db.String(200),)
     jogo = db.Column(db.String(80), nullable = False)
--- app/resources/veiculos.py
+++ app/resources/veiculos.py
@@ -18,32 +18,58 @@ class VeiculoResource(Resource):
         if token_enviado:
             token_retornado, valid = token_enviado
             if valid:
-                veiculos = Veiculo.query.all()
-                if veiculos:
 
-                    lista_veiculos = []
-                    for veiculo in veiculos:
-                        lista_veiculos.append({
+                veiculo_id = request.args.get('id', type=int)
+
+                if veiculo_id:
+
+                    veiculo = Veiculo.query.get(veiculo_id)
+
+                    if veiculo:
+                        return {
                             'id': veiculo.id,
                             'modelo': veiculo.modelo,
                             'marca': veiculo.marca,
                             'tipo': veiculo.tipo,
                             'capacidade': veiculo.capacidade,
+                            'veiculo': veiculo.veiculo,
                             'preco': float(veiculo.preco),
                             'imagem': veiculo.imagem,
                             'jogo': veiculo.jogo,
                             'descricao': veiculo.descricao
-                        })
+                        }, 200
+                    else:
+                        return {'message': 'Veículo não encontrado'}, 404
 
-                    return {'Veículos': lista_veiculos}, 200
                 else:
-                    return {'message': 'Não foram encontrados veiculos'}
+
+                    veiculos = Veiculo.query.all()
+                    if veiculos:
+                        lista_veiculos = []
+                        for veiculo in veiculos:
+                            lista_veiculos.append({
+                                'id': veiculo.id,
+                                'modelo': veiculo.modelo,
+                                'marca': veiculo.marca,
+                                'tipo': veiculo.tipo,
+                                'capacidade': veiculo.capacidade,
+                                'veiculo': veiculo.veiculo,
+                                'preco': float(veiculo.preco),
+                                'imagem': veiculo.imagem,
+                                'jogo': veiculo.jogo,
+                                'descricao': veiculo.descricao
+                            })
+
+                        return {'Veículos': lista_veiculos}, 200
+                    else:
+                        return {'message': 'Não foram encontrados veiculos'}
             else:
                 return {'message': 'O token é inválido'}
         else:
             return {'message': 'O token não foi encontrado'}
 
-    def post(self):
+    @staticmethod
+    def post():
 
         token_recebido = request.headers.get('Authorization')
 
@@ -56,9 +82,11 @@ class VeiculoResource(Resource):
                 payload = request.get_json()
 
                 veiculo = Veiculo(
+                    modelo = payload['modelo'],
                     marca = payload['marca'],
                     tipo = payload['tipo'],
                     capacidade = payload['capacidade'],
+                    veiculo = payload['veiculo'],
                     preco = payload['preco'],
                     imagem = payload['imagem'],
                     jogo = payload['jogo'],
@@ -83,3 +111,70 @@ class VeiculoResource(Resource):
 
         else:
             return {'message': 'O token de autenticação não encontrado'}
+
+    @staticmethod
+    def put():
+
+        token_recebido = request.headers.get('Authorization')
+
+        token_enviado = Token.query.with_entities(Token.token,
+                                                  Token.valid).filter_by(token=token_recebido).first()
+
+        if token_enviado:
+            token_retornado, valid = token_enviado
+            if valid:
+                payload = request.get_json()
+
+                id_veiculo = payload['id']
+                veiculo = Veiculo.query.filter_by(id = id_veiculo).first()
+
+                if veiculo:
+
+                    veiculo.modelo = payload['modelo']
+                    veiculo.marca = payload['marca'],
+                    veiculo.tipo = payload['tipo'],
+                    veiculo.capacidade = payload['capacidade'],
+                    veiculo.veiculo = payload['veiculo'],
+                    veiculo.preco = payload['preco'],
+                    veiculo.imagem = payload['imagem'],
+                    veiculo.jogo = payload['jogo'],
+                    veiculo.descricao = payload['descricao']
+
+                    try:
+                        db.session.commit()
+                        return {'message': 'Veículo atualizado com sucesso'}
+                    except Exception as e:
+                        return {
+                            'message': 'Erro ao atualizar veículo',
+                            'detalhes': str(e)
+                        }
+
+    @staticmethod
+    def delete():
+
+        token_recebido = request.headers.get('Authorization')
+
+        token_enviado = Token.query.with_entities(Token.token,
+                                                  Token.valid).filter_by(token=token_recebido).first()
+
+        if token_enviado:
+            token_retornado, valid = token_enviado
+            if valid:
+                veiculo_id = request.args.get('id', type=int)
+
+                if not veiculo_id:
+                    return {'message': 'É necessário passar o ID do veículo'}
+                else:
+                    veiculo = Veiculo.query.get(veiculo_id)
+
+                    if veiculo:
+                        try:
+                            db.session.delete(veiculo)
+                            db.session.commit()
+
+                            return {'message': 'Veículo excluido com sucesso'}
+                        except Exception as e:
+                            return {
+                                'message': 'Erro ao deletar o veículo',
+                                'detalhes': str(e)
+                            }
